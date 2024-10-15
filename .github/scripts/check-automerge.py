c2b7ae714d5fe3f72c48659a7f3a3de7b86d1501
walnuts1018 <r.juglans.1018@gmail.com>
2024-10-15 13:18:23 +0900

    add
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- .github/scripts/check-automerge.py
+++ .github/scripts/check-automerge.py
@@ -17,10 +17,11 @@ if result.returncode == 0:
     if new_major == old_major and new_minor == old_minor:
         automerge = True
 
+print("Automerge: %s" % automerge)
+print("Old tag: %s.%s.%s" % (old_major, old_minor, old_patch))
+print("New tag: %s.%s.%s" % (new_major, new_minor, new_patch))
+
 with open(os.environ["GITHUB_OUTPUT"], "a") as f :
-    print("Automerge: %s" % automerge)
     print("{0}={1}".format("automerge", automerge), file=f)
-    print("Old tag: %s.%s.%s" % (old_major, old_minor, old_patch))
     print("{0}={1}".format("old_tag", "%s.%s.%s" % (old_major, old_minor, old_patch)), file=f)
-    print("New tag: %s.%s.%s" % (new_major, new_minor, new_patch))
     print("{0}={1}".format("new_tag", "%s.%s.%s" % (new_major, new_minor, new_patch)), file=f)
