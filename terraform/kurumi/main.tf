fb3a16be3979c602cbedaeefcbd294a53fb0599f
walnuts1018 <r.juglans.1018@gmail.com>
2024-11-12 22:52:25 +0900

    terraform
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/kurumi/main.tf
+++ terraform/kurumi/main.tf
@@ -29,3 +29,38 @@ module "minio" {
   source             = "../modules/minio"
   bucket_name_suffix = ""
 }
+
+import {
+  id = "loki-admin"
+  to = module.minio.aws_s3_bucket.loki-admin
+}
+
+import {
+  id = "loki-chunks"
+  to = module.minio.aws_s3_bucket.loki-chunks
+}
+
+import {
+  id = "loki-ruler"
+  to = module.minio.aws_s3_bucket.loki-ruler
+}
+
+import {
+  id = "oekaki-dengon-game"
+  to = module.minio.aws_s3_bucket.oekaki-dengon-game
+}
+
+import {
+  id = "mucaron"
+  to = module.minio.aws_s3_bucket.mucaron
+}
+
+import {
+  id = "tempo"
+  to = module.minio.aws_s3_bucket.tempo
+}
+
+import {
+  id = "zalando-backup"
+  to = module.minio.aws_s3_bucket.zalando-backup
+}
