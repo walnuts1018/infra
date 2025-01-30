1c180c59bb70e8d4a930add96ed8de95b3156483
walnuts1018 <r.juglans.1018@gmail.com>
2025-01-30 10:51:04 +0900

    feat(minio): enable versioning for S3 bucket in tf-state configuration
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/minio/tf-state.tf
+++ terraform/modules/minio/tf-state.tf
@@ -1,3 +1,11 @@
 resource "aws_s3_bucket" "tf-state" {
   bucket = format("tf-state%s", var.bucket_name_suffix)
 }
+
+
+resource "aws_s3_bucket_versioning" "tf-state" {
+  bucket = aws_s3_bucket.tf-state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
