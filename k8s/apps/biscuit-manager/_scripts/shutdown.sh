04d092225aa54bc014222e707479c5b8523a9bdd
walnuts1018 <r.juglans.1018@gmail.com>
2025-10-06 03:47:36 +0900

    install curl in shutdown script
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- k8s/apps/biscuit-manager/_scripts/shutdown.sh
+++ k8s/apps/biscuit-manager/_scripts/shutdown.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+apt-get update
+apt-get install -y \
+    curl
+
 curl -X POST \
     -H "Host: shutdown-manager.local.walnuts.dev" \
     -H "Authorization: Bearer $(cat /var/run/secrets/shutdown-manager.local.walnuts.dev/serviceaccount/token)" \
