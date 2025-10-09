07bbe46663e70307268ade9af11c449dc2527ae0
Walnuts <r.juglans.1018@gmail.com>
2025-10-09 14:28:52 +0900

    Uncomment authorization header in shutdown script

--- k8s/apps/biscuit-manager/_scripts/shutdown.sh
+++ k8s/apps/biscuit-manager/_scripts/shutdown.sh
@@ -6,7 +6,7 @@ apt-get install -y \
 
 curl -X POST \
     -H "Host: shutdown-manager.local.walnuts.dev" \
-#    -H "Authorization: Bearer $(cat /var/run/secrets/shutdown-manager.local.walnuts.dev/serviceaccount/token)" \
+    -H "Authorization: Bearer $(cat /var/run/secrets/shutdown-manager.local.walnuts.dev/serviceaccount/token)" \
     http://192.168.0.158/shutdown -v
 
 echo "biscuit manager shutdown command sent"
