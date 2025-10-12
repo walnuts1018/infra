577e640a024d3b9021da12517f5c872405acb10d
walnuts1018 <r.juglans.1018@gmail.com>
2025-10-12 18:48:41 +0900

    add minio-default-backup
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

---
+++ k8s/apps/minio-default-backup/_scripts/backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+log() {
+    local level="$1"
+    local msg="$2"
+    local timestamp
+    timestamp=$(date '+%Y-%m-%dT%H:%M:%S%z')
+    
+    shift 2
+    local json="{\"level\":\"$level\",\"ts\":$timestamp,\"msg\":\"$msg\""
+    while [[ $# -gt 0 ]]; do
+        json+=",\"$1\":\"$2\""
+        shift 2
+    done
+    echo "$json}"
+}
+
+
+log "info" "Starting backup process"
+
+for BUCKET in $(rclone lsf minio-default: --dirs-only --config=/config/rclone.conf | sed 's/\///g'); do
+    SOURCE_PATH="minio-default:${BUCKET}"
+    DEST_PATH="minio-biscuit:minio-default-backup/${BUCKET}"
+    log "info" "Sync started" source="${SOURCE_PATH}" dest="${DEST_PATH}"
+    rclone sync --config=/config/rclone.conf -v "${SOURCE_PATH}" "${DEST_PATH}"
+    if [[ $? -eq 0 ]]; then
+        log "info" "Sync completed successfully" source="${SOURCE_PATH}" dest="${DEST_PATH}"
+    else
+        log "error" "Sync failed" source="${SOURCE_PATH}" dest="${DEST_PATH}"
+    fi
+done
+
+log "info" "Backup process completed"
