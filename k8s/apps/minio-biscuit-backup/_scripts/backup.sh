5bb4ebc557379d993dad667c1da21e2b719ecbdc
walnuts1018 <r.juglans.1018@gmail.com>
2025-11-13 01:38:17 +0900

    Update backup script to use new B2 destination format
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- k8s/apps/minio-biscuit-backup/_scripts/backup.sh
+++ k8s/apps/minio-biscuit-backup/_scripts/backup.sh
@@ -27,7 +27,7 @@ for BUCKET in $(rclone lsf minio-biscuit: --dirs-only --config=/config/rclone.co
     fi
 
     SOURCE_PATH="minio-biscuit:${BUCKET}"
-    DEST_PATH="b2:walnuts-minio-biscuit-backup-81f18e5/${BUCKET}"
+    DEST_PATH="b2-crypted:${BUCKET}"
     log "info" "Sync started" source "${SOURCE_PATH}" dest "${DEST_PATH}"
     rclone sync --config=/config/rclone.conf -v "${SOURCE_PATH}" "${DEST_PATH}"
     if [[ $? -eq 0 ]]; then
