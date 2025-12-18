0f8df85f84864beb6baaad2333b23ec55aeb1465
walnuts1018 <r.juglans.1018@gmail.com>
2025-12-18 17:36:25 +0900

    Fix shutdown script to use correct hostname for shutdown manager
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- k8s/apps/biscuit-manager/_scripts/shutdown.sh
+++ k8s/apps/biscuit-manager/_scripts/shutdown.sh
@@ -5,8 +5,7 @@ apt-get install -y \
     curl
 
 curl -X POST \
-    -H "Host: shutdown-manager.local.walnuts.dev" \
     -H "Authorization: Bearer $(cat /var/run/secrets/shutdown-manager.local.walnuts.dev/serviceaccount/token)" \
-    http://192.168.0.158/shutdown -v
+    http://shutdown-manager.local.walnuts.dev/shutdown -v
 
 echo "biscuit manager shutdown command sent"
