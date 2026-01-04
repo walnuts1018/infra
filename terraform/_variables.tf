8b229d363e67a118fcf9addc33620797cb4acf9a
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-05 03:22:13 +0900

    fix: remove redundant variable declarations from main.tf
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

---
+++ terraform/_variables.tf
@@ -0,0 +1,11 @@
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "b2_application_key" {
+  type = string
+}
+
+variable "zitadel_jwt_profile_json" {
+  type = string
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,3 @@
-variable "cloudflare_api_token" {
-  type = string
-}
-
-variable "b2_application_key" {
-  type = string
-}
-
-variable "zitadel_jwt_profile_json" {
-  type = string
-}
-
 terraform {
   required_version = "~>1.14.0"
   cloud {
