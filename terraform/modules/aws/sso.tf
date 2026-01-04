31407d00c3d1e3975177ce4d5b370cf617c47b28
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-05 02:40:51 +0900

    fix: rename policy attachments for clarity and update resource names
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/aws/sso.tf
+++ terraform/modules/aws/sso.tf
@@ -5,20 +5,26 @@ resource "aws_ssoadmin_permission_set" "admin" {
   session_duration = "PT12H"
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "admin" {
+resource "aws_ssoadmin_managed_policy_attachment" "administrator" {
   instance_arn       = local.sso_instance_arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "billing" {
+resource "aws_ssoadmin_managed_policy_attachment" "aws_billing_read_only" {
   instance_arn       = local.sso_instance_arn
   managed_policy_arn = "arn:aws:iam::aws:policy/AWSBillingReadOnlyAccess"
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 }
 
-resource "aws_ssoadmin_managed_policy_attachment" "billing_conductor" {
+resource "aws_ssoadmin_managed_policy_attachment" "billing" {
   instance_arn       = local.sso_instance_arn
-  managed_policy_arn = "arn:aws:iam::aws:policy/AWSBillingConductorReadOnlyAccess"
+  managed_policy_arn = "arn:aws:iam::aws:policy/Billing"
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+}
+
+resource "aws_ssoadmin_managed_policy_attachment" "aws_purchase_orders_service_role" {
+  instance_arn       = local.sso_instance_arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AWSPurchaseOrdersServiceRolePolicy"
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 }
