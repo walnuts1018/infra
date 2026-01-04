06f6aa1a89bc1a6c3245541394229b3258ba2d1f
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-04 19:33:11 +0900

    fix: update zitadel project configuration and clean up unused imports
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/zitadel/project.tf
+++ terraform/modules/zitadel/project.tf
@@ -1,4 +1,5 @@
 resource "zitadel_project" "default" {
-  name                     = "walnuts.dev"
-  org_id                   = zitadel_org.ZITADEL.id
+  name                   = "walnuts.dev"
+  org_id                 = zitadel_org.ZITADEL.id
+  project_role_assertion = true
 }
--- terraform/zitadel.tf
+++ terraform/zitadel.tf
@@ -1,14 +1,4 @@
 module "zitadel" {
-  source                = "./modules/zitadel"
+  source           = "./modules/zitadel"
   jwt_profile_json = var.zitadel_jwt_profile_json
-}
-
-import {
-  id = "237477062321897835"
-  to = module.zitadel.zitadel_org.ZITADEL
-}
-
-import {
-  id = "237477822715658605"
-  to = module.zitadel.zitadel_project.default
 }
