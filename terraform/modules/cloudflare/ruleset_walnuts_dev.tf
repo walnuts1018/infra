63ccd6bb40698d5b4a54eda912c53a5db22e61b0
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-04 18:45:49 +0900

    fix: remove unnecessary enabled flags and update expressions in Cloudflare rulesets
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/cloudflare/ruleset_walnuts_dev.tf
+++ terraform/modules/cloudflare/ruleset_walnuts_dev.tf
@@ -7,8 +7,6 @@ resource "cloudflare_ruleset" "configuration_rules_walnuts_dev" {
     {
       action      = "set_config"
       description = "enable Rocket Loader"
-      enabled     = true
-      ref         = "9c1ef58603494a50af7855c3263e6bdf"
 
       expression = "(http.host eq \"walnuts.dev\") or (http.host eq \"minio.walnuts.dev\") or (http.host eq \"oekaki.walnuts.dev\")"
       action_parameters = {
@@ -18,8 +16,6 @@ resource "cloudflare_ruleset" "configuration_rules_walnuts_dev" {
     {
       action      = "set_config"
       description = "disable zalaz"
-      enabled     = true
-      ref         = "f828c6290c354dec96cff72dd0d17005"
 
       expression = "(http.request.full_uri wildcard r\"https://hedgedoc.walnuts.dev/*\")"
       action_parameters = {
@@ -38,10 +34,8 @@ resource "cloudflare_ruleset" "cache_rules_walnuts_dev" {
     {
       action      = "set_cache_settings"
       description = "walnuts.dev"
-      enabled     = true
-      ref         = "02afb6686434455195ad5e1d630a099d"
 
-      expression = "(http.host eq \"walnuts.dev\") or (http.host eq \"oekaki.walnuts.dev\") or (http.host eq \"maple.walnuts.dev\")"
+      expression = "(http.host eq \"walnuts.dev\") or (http.host eq \"oekaki.walnuts.dev\") or (http.host eq \"teddy.walnuts.dev\")"
       action_parameters = {
         cache = true
       }
@@ -49,8 +43,6 @@ resource "cloudflare_ruleset" "cache_rules_walnuts_dev" {
     {
       action      = "set_cache_settings"
       description = "misskey"
-      enabled     = true
-      ref         = "e6dbe87b1b2b483db3df88b5576deb03"
 
       expression = "(http.host eq \"misskey.walnuts.dev\" and starts_with(http.request.uri, \"/api/\"))"
 
@@ -61,8 +53,6 @@ resource "cloudflare_ruleset" "cache_rules_walnuts_dev" {
     {
       action      = "set_cache_settings"
       description = "minio"
-      enabled     = true
-      ref         = "9f4de8f107314fbe8058a07b62e1ffcd"
 
       expression = "(http.host wildcard \"minio.walnuts.dev\") or (http.host wildcard \"minio-biscuit.walnuts.dev\")"
       action_parameters = {
