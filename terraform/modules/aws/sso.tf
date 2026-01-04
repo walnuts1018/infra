d232dbee517e0758817a0aecb631459f1c062db6
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-05 02:37:03 +0900

    fix: add AWSBillingConductorReadOnlyAccess policy attachment to admin permission set
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/aws/sso.tf
+++ terraform/modules/aws/sso.tf
@@ -16,3 +16,9 @@ resource "aws_ssoadmin_managed_policy_attachment" "billing" {
   managed_policy_arn = "arn:aws:iam::aws:policy/AWSBillingReadOnlyAccess"
   permission_set_arn = aws_ssoadmin_permission_set.admin.arn
 }
+
+resource "aws_ssoadmin_managed_policy_attachment" "billing_conductor" {
+  instance_arn       = local.sso_instance_arn
+  managed_policy_arn = "arn:aws:iam::aws:policy/AWSBillingConductorReadOnlyAccess"
+  permission_set_arn = aws_ssoadmin_permission_set.admin.arn
+}
