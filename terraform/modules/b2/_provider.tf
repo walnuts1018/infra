390e0b3c40a5337c293c5a4417b8d7a427dcf54b
walnuts1018 <r.juglans.1018@gmail.com>
2026-01-05 19:20:40 +0900

    fix: update provider version constraints for aws and b2 modules
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/b2/_provider.tf
+++ terraform/modules/b2/_provider.tf
@@ -1,8 +1,8 @@
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     b2 = {
-      source = "Backblaze/b2"
+      source  = "Backblaze/b2"
+      version = "0.12.0"
     }
   }
 }
