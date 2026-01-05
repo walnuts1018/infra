e40558aa26454d498145ed7dc92d967a6e334e97
Walnuts <r.juglans.1018@gmail.com>
2026-01-05 13:53:39 +0900

    Add boot volume size to compute instance configuration
    Added boot volume size configuration for compute instance.

--- terraform/modules/oci/compute.tf
+++ terraform/modules/oci/compute.tf
@@ -9,8 +9,9 @@ resource "oci_core_instance" "orange" {
   }
 
   source_details {
-    source_id   = "ocid1.image.oc1.ap-osaka-1.aaaaaaaawzfbc5pjimseh6eisfqhfztalzx46h5bhntvxomckmulk7hqtyoa"
-    source_type = "image"
+    source_id               = "ocid1.image.oc1.ap-osaka-1.aaaaaaaawzfbc5pjimseh6eisfqhfztalzx46h5bhntvxomckmulk7hqtyoa"
+    source_type             = "image"
+    boot_volume_size_in_gbs = 200
   }
 
   display_name = "orange"
