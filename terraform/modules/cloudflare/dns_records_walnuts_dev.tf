9afa9512879f7bb73237cdc0a4c15a64a5386455
walnuts1018 <r.juglans.1018@gmail.com>
2026-02-17 17:36:44 +0900

    add DNS records for email configuration in Cloudflare
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/cloudflare/dns_records_walnuts_dev.tf
+++ terraform/modules/cloudflare/dns_records_walnuts_dev.tf
@@ -51,42 +51,6 @@ resource "cloudflare_dns_record" "www_walnuts_dev" {
   }
 }
 
-resource "cloudflare_dns_record" "smtp_send_resend_walnuts_dev" {
-  content  = "feedback-smtp.us-east-1.amazonses.com"
-  name     = "send.resend.walnuts.dev"
-  priority = 10
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  zone_id  = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "dmarc" {
-  content = "\"v=DMARC1; p=none; rua=mailto:5e9239fe52ad41fd850bd72545e1e484@dmarc-reports.cloudflare.net;\""
-  name    = "_dmarc.walnuts.dev"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  zone_id = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "resend_domainkey_walnuts_dev" {
-  content = "\"p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUe5ntBGI0Xnq8SBPdRqtCD7ZFiI39jCB9NbSOatnYw8MufwpaLPBTMwKdPKKWx+w9Ytv8LRQo1hbj6vGfjPq5mZ1wJPcA6YontVaVpXrL933pb9FYDCzoS3apPsQe3aYsRYA/vjvp6IU19PTVq4NTnX9SFUHK5i7eD8qUlevpvwIDAQAB\""
-  name    = "resend._domainkey.resend.walnuts.dev"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  zone_id = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "spf_send_resend_walnuts_dev" {
-  content = "\"v=spf1 include:amazonses.com ~all\""
-  name    = "send.resend.walnuts.dev"
-  proxied = false
-  ttl     = 1
-  type    = "TXT"
-  zone_id = cloudflare_zone.walnuts_dev.id
-}
 
 resource "cloudflare_dns_record" "visual_studio_marketplace_walnuts_dev" {
   content = "\"81a38dcc-dcaf-4c0c-b029-b215d8a3c67c\""
@@ -114,5 +78,3 @@ resource "cloudflare_dns_record" "google_site_verification_walnuts_dev" {
   type    = "TXT"
   zone_id = cloudflare_zone.walnuts_dev.id
 }
-
-
