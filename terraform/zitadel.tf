b4619dcfba979a51fb821d08f6b60f34501945ea
walnuts1018 <r.juglans.1018@gmail.com>
2026-06-28 18:16:48 +0900

    Refactor Zitadel module: remove moved block for project resource transition
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/zitadel.tf
+++ terraform/zitadel.tf
@@ -4,8 +4,3 @@ module "zitadel" {
   google_idp_client_secret = var.zitadel_google_idp_client_secret
   # github_idp_client_secret = var.zitadel_github_idp_client_secret
 }
-
-moved {
-  from = module.zitadel.zitadel_project.default
-  to   = module.zitadel.zitadel_project.walnuts_dev
-}
