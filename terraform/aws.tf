c8eed29d02ecccf07927efeea68188e561076e4d
walnuts1018 <r.juglans.1018@gmail.com>
2026-07-26 02:42:46 +0900

    update: add SES SMTP user and policy configuration; include sensitive output for username and password
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -9,3 +9,13 @@ output "amazonses_verification_token_walnuts_dev" {
 output "amazonses_dkim_tokens_walnuts_dev" {
   value = module.aws.ses_dkim_tokens_walnuts_dev
 }
+
+output "stalwart_ses_smtp_username" {
+  value     = module.aws.stalwart_ses_smtp_username
+  sensitive = true
+}
+
+output "stalwart_ses_smtp_password" {
+  value     = module.aws.stalwart_ses_smtp_password
+  sensitive = true
+}
--- terraform/modules/aws/ses.tf
+++ terraform/modules/aws/ses.tf
@@ -22,3 +22,41 @@ resource "aws_ses_domain_mail_from" "walnuts_dev" {
   domain           = aws_ses_domain_identity.walnuts_dev.domain
   mail_from_domain = "mail.${aws_ses_domain_identity.walnuts_dev.domain}"
 }
+
+data "aws_iam_policy_document" "stalwart_ses_smtp" {
+  statement {
+    actions = ["ses:SendRawEmail"]
+
+    resources = [aws_ses_domain_identity.walnuts_dev.arn]
+
+    condition {
+      test     = "StringLike"
+      variable = "ses:FromAddress"
+      values   = ["*@${aws_ses_domain_identity.walnuts_dev.domain}"]
+    }
+  }
+}
+
+resource "aws_iam_user" "stalwart_ses_smtp" {
+  name = "stalwart-ses-smtp"
+}
+
+resource "aws_iam_user_policy" "stalwart_ses_smtp" {
+  name   = "ses-send-raw-email"
+  user   = aws_iam_user.stalwart_ses_smtp.name
+  policy = data.aws_iam_policy_document.stalwart_ses_smtp.json
+}
+
+resource "aws_iam_access_key" "stalwart_ses_smtp" {
+  user = aws_iam_user.stalwart_ses_smtp.name
+}
+
+output "stalwart_ses_smtp_username" {
+  value     = aws_iam_access_key.stalwart_ses_smtp.id
+  sensitive = true
+}
+
+output "stalwart_ses_smtp_password" {
+  value     = aws_iam_access_key.stalwart_ses_smtp.ses_smtp_password_v4
+  sensitive = true
+}
