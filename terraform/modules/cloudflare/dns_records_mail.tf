335cdc44679fbbe53191ef2c17b180133de25d01
walnuts1018 <r.juglans.1018@gmail.com>
2026-07-26 04:36:04 +0900

    Refactor DNS records for domain authentication and Amazon SES configuration
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/cloudflare/dns_records_mail.tf
+++ terraform/modules/cloudflare/dns_records_mail.tf
@@ -1,27 +1,27 @@
 # ----------------------------------------------
-# SPF / DKIM / DMARC
+# Domain authentication
 # ----------------------------------------------
 
 resource "cloudflare_dns_record" "spf_walnuts_dev" {
   zone_id = cloudflare_zone.walnuts_dev.id
   name    = "walnuts.dev"
-  content = "v=spf1 include:amazonses.com include:_spf.mx.cloudflare.net ~all"
+  content = "v=spf1 include:amazonses.com -all"
   type    = "TXT"
   ttl     = 600
   proxied = false
 }
 
-resource "cloudflare_dns_record" "dmarc" {
+resource "cloudflare_dns_record" "dmarc_walnuts_dev" {
   zone_id = cloudflare_zone.walnuts_dev.id
   name    = "_dmarc.walnuts.dev"
-  content = "v=DMARC1; p=none; rua=mailto:dmarcreports@walnuts.dev;"
+  content = "v=DMARC1; p=quarantine; adkim=s; aspf=s; rua=mailto:dmarcreports@walnuts.dev"
   type    = "TXT"
-  ttl     = 1
+  ttl     = 600
   proxied = false
 }
 
 # ----------------------------------------------
-# Amazon SES
+# Amazon SES outbound delivery
 # ----------------------------------------------
 
 resource "cloudflare_dns_record" "amazonses_verification_record_walnuts_dev" {
@@ -34,15 +34,16 @@ resource "cloudflare_dns_record" "amazonses_verification_record_walnuts_dev" {
 }
 
 resource "cloudflare_dns_record" "amazonses_dkim_walnuts_dev" {
-  count   = 3
+  count   = length(var.amazonses_dkim_tokens_walnuts_dev)
   zone_id = cloudflare_zone.walnuts_dev.id
   name    = "${var.amazonses_dkim_tokens_walnuts_dev[count.index]}._domainkey.walnuts.dev"
+  content = "${var.amazonses_dkim_tokens_walnuts_dev[count.index]}.dkim.amazonses.com"
   type    = "CNAME"
   ttl     = 600
   proxied = false
-  content = "${var.amazonses_dkim_tokens_walnuts_dev[count.index]}.dkim.amazonses.com"
 }
 
+# `mail.walnuts.dev` is reserved for SES's envelope sender and must not receive mail.
 resource "cloudflare_dns_record" "amazonses_mail_from_mx_walnuts_dev" {
   zone_id  = cloudflare_zone.walnuts_dev.id
   name     = "mail.walnuts.dev"
@@ -56,8 +57,61 @@ resource "cloudflare_dns_record" "amazonses_mail_from_mx_walnuts_dev" {
 resource "cloudflare_dns_record" "spf_mail_walnuts_dev" {
   zone_id = cloudflare_zone.walnuts_dev.id
   name    = "mail.walnuts.dev"
-  content = "\"v=spf1 include:amazonses.com ~all\""
+  content = "v=spf1 include:amazonses.com -all"
   type    = "TXT"
   ttl     = 600
   proxied = false
+}
+
+# ----------------------------------------------
+# Stalwart inbound delivery and client discovery
+# ----------------------------------------------
+
+resource "cloudflare_dns_record" "stalwart_a" {
+  zone_id = cloudflare_zone.walnuts_dev.id
+  name    = "mx.walnuts.dev"
+  content = "111.100.165.117"
+  type    = "A"
+  ttl     = 600
+  proxied = false
+}
+
+resource "cloudflare_dns_record" "mx_walnuts_dev" {
+  zone_id  = cloudflare_zone.walnuts_dev.id
+  name     = "walnuts.dev"
+  content  = "mx.walnuts.dev"
+  type     = "MX"
+  priority = 10
+  ttl      = 600
+  proxied  = false
+}
+
+resource "cloudflare_dns_record" "srv_imaps" {
+  zone_id  = cloudflare_zone.walnuts_dev.id
+  name     = "_imaps._tcp.walnuts.dev"
+  type     = "SRV"
+  ttl      = 600
+  priority = 10
+  proxied  = false
+  content  = "10 10 993 mx.walnuts.dev"
+}
+
+resource "cloudflare_dns_record" "srv_submission" {
+  zone_id  = cloudflare_zone.walnuts_dev.id
+  name     = "_submission._tcp.walnuts.dev"
+  type     = "SRV"
+  ttl      = 600
+  priority = 10
+  proxied  = false
+  content  = "10 10 587 mx.walnuts.dev"
+}
+
+resource "cloudflare_dns_record" "srv_smtps" {
+  zone_id  = cloudflare_zone.walnuts_dev.id
+  name     = "_smtps._tcp.walnuts.dev"
+  type     = "SRV"
+  ttl      = 600
+  priority = 10
+  proxied  = false
+  content  = "10 10 465 mx.walnuts.dev"
 }
