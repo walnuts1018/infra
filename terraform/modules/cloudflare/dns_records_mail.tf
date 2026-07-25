d2113b645d20fe33109c8ffc4a0d090f7df0d4f2
walnuts1018 <r.juglans.1018@gmail.com>
2026-07-26 04:31:39 +0900

    Remove unused Cloudflare email routing and DKIM records
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- terraform/modules/cloudflare/dns_records_mail.tf
+++ terraform/modules/cloudflare/dns_records_mail.tf
@@ -21,49 +21,6 @@ resource "cloudflare_dns_record" "dmarc" {
 }
 
 # ----------------------------------------------
-# Cloudflare Email Routing
-# ----------------------------------------------
-
-resource "cloudflare_dns_record" "mx_cloudflare_1_walnuts_dev" {
-  content  = "route1.mx.cloudflare.net"
-  name     = "walnuts.dev"
-  priority = 110
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  zone_id  = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "mx_cloudflare_2_walnuts_dev" {
-  content  = "route2.mx.cloudflare.net"
-  name     = "walnuts.dev"
-  priority = 120
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  zone_id  = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "mx_cloudflare_3_walnuts_dev" {
-  content  = "route3.mx.cloudflare.net"
-  name     = "walnuts.dev"
-  priority = 130
-  proxied  = false
-  ttl      = 1
-  type     = "MX"
-  zone_id  = cloudflare_zone.walnuts_dev.id
-}
-
-resource "cloudflare_dns_record" "dkim_cloudflare_walnuts_dev" {
-  zone_id = cloudflare_zone.walnuts_dev.id
-  name    = "cf2024-1._domainkey.walnuts.dev"
-  content = "\"v=DKIM1; h=sha256; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiweykoi+o48IOGuP7GR3X0MOExCUDY/BCRHoWBnh3rChl7WhdyCxW3jgq1daEjPPqoi7sJvdg5hEQVsgVRQP4DcnQDVjGMbASQtrY4WmB1VebF+RPJB2ECPsEDTpeiI5ZyUAwJaVX7r6bznU67g7LvFq35yIo4sdlmtZGV+i0H4cpYH9+3JJ78k\" \"m4KXwaf9xUJCWF6nxeD+qG6Fyruw1Qlbds2r85U9dkNDVAS3gioCvELryh1TxKGiVTkg4wqHTyHfWsp7KD3WQHYJn0RyfJJu6YEmL77zonn7p2SRMvTMP3ZEXibnC9gz3nnhR6wcYL8Q7zXypKTMD58bTixDSJwIDAQAB\""
-  type    = "TXT"
-  ttl     = 1
-  proxied = false
-}
-
-# ----------------------------------------------
 # Amazon SES
 # ----------------------------------------------
 
