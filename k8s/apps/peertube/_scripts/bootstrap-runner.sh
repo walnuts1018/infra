c6cdc9bf12449a569ed38b12bbf5167ff04264d6
walnuts1018 <r.juglans.1018@gmail.com>
2026-09-20 18:46:49 +0900

    Fix export order of REGISTRATION_TOKEN in bootstrap script for clarity
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- k8s/apps/peertube/_scripts/bootstrap-runner.sh
+++ k8s/apps/peertube/_scripts/bootstrap-runner.sh
@@ -4,5 +4,6 @@ set -eu
 token_file='/runner-bootstrap/registration-token'
 test -s "${token_file}"
 
-export REGISTRATION_TOKEN="$(cat "${token_file}")"
+REGISTRATION_TOKEN="$(cat "${token_file}")"
+export REGISTRATION_TOKEN
 exec peertube-runner bootstrap
